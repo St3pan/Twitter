5a90ca3f3c72b018657a091c194d2e4883392904
Stepan Khylko <stepankhylko@ukr.net>
2021-03-24 12:36:46 +0200

    home page styles

--- app.js
+++ app.js
@@ -27,9 +27,11 @@ app.use(
 // Routes
 const loginRoute = require("./routes/loginRoutes");
 const registerRoute = require("./routes/registerRoutes");
+const logout = require("./routes/logout");
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
+app.use("/logout", logout);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
   const payload = {
